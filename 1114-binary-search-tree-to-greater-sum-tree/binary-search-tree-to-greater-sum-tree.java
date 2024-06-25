328243e50ebcd8bbc25fe07b3b0ec854ba9106ee
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-06-25 07:58:51 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (93.86%) - LeetSync

---
+++ 1114-binary-search-tree-to-greater-sum-tree/binary-search-tree-to-greater-sum-tree.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int traverse(TreeNode root,int carry){
+        if(root==null) return carry;
+        carry=traverse(root.right,carry);
+        root.val+=carry;
+        carry=root.val;
+        return traverse(root.left,carry);
+    }
+    public TreeNode bstToGst(TreeNode root) {
+        traverse(root,0);
+        return root;
+    }
+}
