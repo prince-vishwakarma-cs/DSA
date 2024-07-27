ec94277a80347962bb2024ccf248e31a74d57223
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-07-27 22:20:28 +0530

    Time: 8 ms (37.94%) | Memory: 50.2 MB (5.23%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,24 +1,23 @@
 class Solution {
-    public int maximum(int[] arr,int i,int canBuy,int k,Map<String,Integer> mp){
+    public int maximum(int[] arr,int i,int canBuy,int k,Integer[][][] dp){
         if(i>=arr.length || k==0) return 0;
 
-String key = i + "can" + canBuy + "hi" + k;
-if(mp.containsKey(key)) return mp.get(key);
+if(dp[i][canBuy][k] !=null ) return dp[i][canBuy][k];
 int max=0;
 if(canBuy==1){
-    int buy=maximum(arr,i+1,0,k,mp)-arr[i];
-    int nobuy=maximum(arr,i+1,1,k,mp);
+    int buy=maximum(arr,i+1,0,k,dp)-arr[i];
+    int nobuy=maximum(arr,i+1,1,k,dp);
     max=Math.max(buy,nobuy);
 } else{
-    int sell=maximum(arr,i+1,1,k-1,mp)+arr[i];
-    int nosell=maximum(arr,i+1,0,k,mp);
+    int sell=maximum(arr,i+1,1,k-1,dp)+arr[i];
+    int nosell=maximum(arr,i+1,0,k,dp);
     max=Math.max(sell,nosell);
 }
-mp.put(key,max);
+dp[i][canBuy][k] = max;
 return max;
     }
     public int maxProfit(int k, int[] prices){
-        Map<String,Integer> mp=new HashMap<>();
-        return Math.max(0,maximum(prices,0,1,k,mp));
+        Integer[][][] dp=new Integer[prices.length][2][k+1];
+        return Math.max(0,maximum(prices,0,1,k,dp));
     }
 }
