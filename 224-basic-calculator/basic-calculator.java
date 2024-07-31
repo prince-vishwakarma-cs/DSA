5adb229ff541f66dbca564a973aed090bd459dd0
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-07-31 22:57:30 +0530

    Time: 1 ms (100.00%) | Memory: 43.2 MB (94.45%) - LeetSync

---
+++ 224-basic-calculator/basic-calculator.java
@@ -0,0 +1,26 @@
+class Solution {
+    int i=0;
+    public int calculate(String s) {
+           return calc(s);
+    }
+    private int calc(String s) {
+        int num = 0, ans = 0;
+        int sign = 1;
+       while (i<s.length()) {
+            char ch = s.charAt(i++);
+           if (ch>='0' && ch<='9') {
+                num = num*10+ch-'0';
+            } else if (ch=='(') {
+               num = calc(s);
+            } else if (ch==')') {
+                return ans+=num*sign;
+           } else if (ch=='+' || ch=='-') {
+                ans+=num*sign;
+                num = 0;
+                sign = (ch=='-')?-1:1;
+            }
+        }
+        ans+=num*sign;
+        return ans;
+   }
+}
