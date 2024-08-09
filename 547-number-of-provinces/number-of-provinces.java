5d6c23f9ecaf9be41eeaaa3a5675fdbcaed3772c
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-08-09 10:24:18 +0530

    Time: 1 ms (91.21%) | Memory: 47.7 MB (52.11%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,22 +1,21 @@
 class Solution {
-    public void helper(int[][] arr, boolean[] visited, int row) {
-        visited[row] = true;
-        for (int col = 0; col < arr.length; col++) {
-            if (arr[row][col] == 1 && !visited[col]) {
-                helper(arr, visited, col);
+    public void ans(int[][] isConnected,int i,boolean[] visited){
+        visited[i] =true;
+        for(int j=0;j<isConnected[i].length;j++){
+            if(isConnected[i][j]==1 && !visited[j]){
+                ans(isConnected,j,visited);
             }
         }
     }
-
     public int findCircleNum(int[][] isConnected) {
-        int s = 0;
-        boolean[] visited = new boolean[isConnected.length];
-        for (int i = 0; i < isConnected.length; i++) {
-            if (!visited[i]) {
-                s++;
-                helper(isConnected, visited, i);
+        int ans=0;
+        boolean[] visited=new boolean[isConnected.length];
+        for(int i=0;i<isConnected.length;i++){
+            if(!visited[i]){
+                ans(isConnected,i,visited);
+                ans++;
             }
         }
-        return s;
+        return ans;
     }
 }
