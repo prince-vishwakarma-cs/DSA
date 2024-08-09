afa69628524fa38d1e1893fd91bdce686ac62a8b
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-08-09 13:08:04 +0530

    Time: 2 ms (12.23%) | Memory: 44.6 MB (79.25%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,35 +1,25 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int m=image.length;
+        int n=image[0].length;
+        boolean [][] visited = new boolean[m][n];
         Queue<int[]> q=new LinkedList<>();
-        boolean[][] visited=new boolean[image.length][image[0].length];
-        int rang=image[sr][sc];
         q.add(new int[]{sr,sc});
-        visited[sr][sc]=true;
+        int dir[][] ={{0,1},{0,-1},{1,0},{-1,0}};
         while(!q.isEmpty()){
-            int r=q.peek()[0];
-            int c=q.peek()[1];
-            q.poll();
-            if(c<image[0].length-1 && image[r][c+1]==rang && !visited[r][c+1]){
-                visited[r][c+1]=true;
-                q.add(new int[]{r,c+1});
+            int[] k=q.poll();
+            visited[k[0]][k[1]]=true;
+            for(int p=0;p<dir.length;p++){
+                int i=k[0]+dir[p][0];
+                int j=k[1]+dir[p][1];
+                if(i>=0 && j>=0 && i<image.length && j<image[0].length && !visited[i][j] && image[i][j]==image[k[0]][k[1]]){
+                    q.add(new int[]{i,j});
+                }
             }
-            if(r>0 && image[r-1][c]==rang && !visited[r-1][c]){
-                visited[r-1][c]=true;
-                q.add(new int[]{r-1,c});
-            }
-            if(r<image.length-1 && image[r+1][c]==rang && !visited[r+1][c]){
-                visited[r+1][c]=true;
-                q.add(new int[]{r+1,c});
-            }
-            if(c>0 && image[r][c-1]==rang && !visited[r][c-1]){
-                visited[r][c-1]=true;
-                q.add(new int[]{r,c-1});
-            }
-            
         }
         for(int i=0;i<image.length;i++){
             for(int j=0;j<image[0].length;j++){
-                if(visited[i][j]) image[i][j]=color;
+                if(visited[i][j]) image[i][j] = color;
             }
         }
         return image;
