9717ae440d08cdbf494b73f2087ef3c38660bead
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-08-09 12:27:42 +0530

    Time: 2 ms (72.12%) | Memory: 42.1 MB (83.42%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,47 +2,50 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-        int[][] visited = grid;
+        int ans = 0;
         Queue<int[]> q = new LinkedList<>();
-        int countFreshOrange = 0;
+        boolean[][] visited = new boolean[m][n];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (visited[i][j] == 2) {
-                    q.offer(new int[] {i, j});
-                }
-                if (visited[i][j] == 1) {
-                    countFreshOrange++;
-                }
+                if (grid[i][j] == 2 && !visited[i][j]) q.add(new int[] { i, j });
             }
         }
-        if (countFreshOrange == 0)
-            return 0;
-        if (q.isEmpty())
-            return -1;
-        
-        int minutes = -1;
-        int[][] dirs = {{1, 0},{-1, 0},{0, -1},{0, 1}};
         while (!q.isEmpty()) {
             int size = q.size();
-            while (size-- > 0) {
-                int[] cell = q.poll();
-                int x = cell[0];
-                int y = cell[1];
-                for (int[] dir : dirs) {
-                    int i = x + dir[0];
-                    int j = y + dir[1];
-                    if (i >= 0 && i < m && j >= 0 && j < n && visited[i][j] == 1) {
-                        visited[i][j] = 2;
-                        countFreshOrange--;
-                        q.offer(new int[] {i, j});
-                    }
+            boolean anychange=false;
+            for (int p = 0; p < size; p++) {
+                int[] k = q.poll();
+                int i=k[0],j=k[1];
+                visited[i][j] = true;
+                if (i-1 >= 0 && grid[i-1][j] == 1 && !visited[i-1][j]) {
+                    q.add(new int[] {i-1,j});
+                    grid[i-1][j] = 2;
+                    anychange=true;
+                }
+                if (i+1 < m && grid[i+1][j] == 1 && !visited[i+1][j]) {
+                    q.add(new int[] {i+1,j});
+                    grid[i+1][j] = 2;
+                    anychange=true;
+                }
+                if (j-1 >= 0 && grid[i][j-1] == 1 && !visited[i][j-1]) {
+                    q.add(new int[] {i,j-1});
+                    grid[i][j-1] = 2;
+                    anychange=true;
+                }
+                if (j+1 < n && grid[i][j+1] == 1 && !visited[i][j+1]) {
+                    q.add(new int[] {i,j+1});
+                    grid[i][j+1] = 2;
+                    anychange=true;
                 }
             }
-            minutes++;
+           if(anychange) ans++;
         }
-        
-        if (countFreshOrange == 0)
-            return minutes;
-        return -1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && !visited[i][j])
+                    return -1;
+            }
+        }
+        return ans;
     }
 }
