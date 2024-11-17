aa63c2c9f03cae094ba98f1f0f47cfa5ac787c8f
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-11-17 12:26:57 +0530

    Time: 7 ms (100.00%) | Memory: 49.1 MB (77.40%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.java
+++ 303-range-sum-query-immutable/range-sum-query-immutable.java
@@ -1,34 +1,19 @@
 class NumArray {
-    int tree[];
-    int n;
-    int arr[];
-
+    int[] prefixSum;
     public NumArray(int[] nums) {
-        n=nums.length;
-        arr=nums;
-        tree=new int[n*4];
-        buildtree(0,0,n-1,nums);
-    }
-    
-    public int sumRange(int left, int right) {
-        return findintree(0,0,n-1,left,right,arr);
-    }
-
-    public void buildtree(int i,int l,int r,int[] num){
-        if(l==r){
-            tree[i]=num[l];
-            return;
+        for(int i=1;i<nums.length;i++){
+            nums[i]+=nums[i-1];
         }
-        int mid=l+(r-l)/2;
-        buildtree(2*i+1,l,mid,num);
-        buildtree(2*i+2,mid+1,r,num);
-        tree[i]=tree[2*i+1]+tree[2*i+2];
+        prefixSum=nums;
     }
-
-    public int findintree(int i,int l,int r,int st,int end,int[] num){
-        if(l>end || r<st) return 0;
-        if(l>=st && r<=end) return tree[i];
-        int mid=l+(r-l)/2;
-        return findintree(2*i+1,l,mid,st,end,num) + findintree(2*i+2,mid+1,r,st,end,num);
+    public int sumRange(int left, int right) {
+        if(left>0) return prefixSum[right]-prefixSum[left-1];
+        return prefixSum[right];
     }
 }
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
