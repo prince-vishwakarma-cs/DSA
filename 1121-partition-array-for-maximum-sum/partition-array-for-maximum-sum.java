1860efb7708b5c08e76cf6b5a8511f2796be00f5
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-12-02 12:01:46 +0530

    Time: 9 ms (14.60%) | Memory: 42.1 MB (53.57%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.java
@@ -1,18 +1,14 @@
 class Solution {
-    public int solve(int[] arr,int k,int i,int[] dp){
-        if(i<0) return 0;
-        if(dp[i]!=-1) return dp[i];
-        int max=0;
-        int val=0;
-        for(int s=i;s>=Math.max(i-k+1,0);s--){
-            val=Math.max(val,arr[s]);
-            max=Math.max(max,(i-s+1)*val+solve(arr,k,s-1,dp));
-        }
-        return dp[i]=max;
-    }
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int[] dp=new int[arr.length];
-        Arrays.fill(dp,-1);
-        return solve(arr,k,arr.length-1,dp);
+        int[] dp = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            dp[i] = 0;
+            int val = 0;
+            for (int s = i; s >= Math.max(i - k + 1, 0); s--) {
+                val = Math.max(val, arr[s]);
+                dp[i] = Math.max(dp[i], (i - s + 1) * val + (s > 0 ? dp[s - 1] : 0));
+            }
+        }
+        return dp[arr.length - 1];
     }
 }
