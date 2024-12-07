c1ce373a4933bc5993941713e63750ac64a0c577
Prince Vishwakarma <117573703+prince-vishwakarma-cs@users.noreply.github.com>
2024-12-07 09:48:18 +0530

    Time: 1 ms (23.13%) | Memory: 40.9 MB (99.02%) - LeetSync

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int strStr(String haystack, String needle) {
+        int n = haystack.length();
+        int m = needle.length();
+        if (m == 0) return 0;
+        if (m > n) return -1;
+
+        int[] pow = new int[n];
+        int[] hash = new int[n];
+        int mod = 1000000007;
+
+        pow[0] = 1;
+        for (int i = 1; i < n; i++) {
+            pow[i] = (int)((long)pow[i - 1] * 31 % mod);
+        }
+
+        hash[0] = haystack.charAt(0) - 'a' + 1;
+        for (int i = 1; i < n; i++) {
+            hash[i] = (hash[i - 1] + (int)((long)pow[i] * (haystack.charAt(i) - 'a' + 1) % mod)) % mod;
+        }
+
+        int hash2 = 0;
+        for (int i = 0; i < m; i++) {
+            hash2 = (hash2 + (int)((long)pow[i] * (needle.charAt(i) - 'a' + 1) % mod)) % mod;
+        }
+
+        for (int i = 0; i <= n - m; i++) {
+            int currHash = (hash[i + m - 1] - (i > 0 ? hash[i - 1] : 0) + mod) % mod;
+            if (currHash == (int)((long)hash2 * pow[i] % mod)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
