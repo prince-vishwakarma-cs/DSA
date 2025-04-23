5c8e108810aeef092b863b303b5a0dfa133cd17f
Prince Vishwakarma <Pv913177@gmail.com>
2025-04-23 20:51:21 +0530

    Time: 8 ms (55.91%) | Memory: 40.4 MB (90.68%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -1,23 +1,26 @@
-class Solution {
-    public int sum(int k){
+public class Solution {
+    public int sum(int k) {
         int total = 0;
-        while(k>0){
-            total+=k%10;
-            k/=10;
+        while (k > 0) {
+            total += k % 10;
+            k /= 10;
         }
         return total;
     }
+
     public int countLargestGroup(int n) {
-        Map<Integer,Integer> mp =new HashMap<>();
-        int maxCount=0;
-        for(int i=1;i<=n;i++){
-            int sum = sum(i);
-            mp.put(sum,mp.getOrDefault(sum,0)+1);
-            if(mp.get(sum)>maxCount) maxCount=mp.get(sum);
+        int[] group = new int[46]; 
+        int maxCount = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int s = sum(i);
+            group[s]++;
+            maxCount = Math.max(maxCount, group[s]);
         }
-        int count=0;
-        for(Map.Entry<Integer, Integer> entry : mp.entrySet()){
-            if(entry.getValue()==maxCount) count++;
+
+        int count = 0;
+        for (int freq : group) {
+            if (freq == maxCount) count++;
         }
         return count;
     }
