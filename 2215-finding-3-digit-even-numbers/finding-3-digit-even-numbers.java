d3da7f07964f530e9c0f29b7741ed442789e2005
Prince Vishwakarma <Pv913177@gmail.com>
2025-05-12 09:01:54 +0530

    Time: 4 ms (96.05%) | Memory: 44.5 MB (95.59%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.java
@@ -1,26 +1,29 @@
 class Solution {
     public int[] findEvenNumbers(int[] digits) {
-        int n =digits.length;
-        Set<Integer> set =new HashSet<>();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(j==i) continue;
-                for(int k=0;k<n;k++){
-                    if(k==j || k==i) continue;
-                    if(digits[i]==0) continue;
-                    if(digits[k]%2==1) continue;
-                    set.add(digits[i]*100+digits[j]*10+digits[k]);
+        int[] freq = new int[10];
+        List<Integer> lt =new ArrayList<>();
+        for(int el:digits) freq[el]++;
+        for(int i=1;i<=9;i++){
+            if(freq[i]==0) continue;
+            freq[i]--;
+            for(int j=0;j<=9;j++){
+                if(freq[j]==0) continue;
+                freq[j]--;
+                for(int k=0;k<=8;k+=2){
+                    if(freq[k]==0) continue;
+                    freq[k]--;
+                    lt.add(i*100+j*10+k);
+                    freq[k]++;
                 }
+                freq[j]++;
             }
+            freq[i]++;
         }
-        int[] arr = new int[set.size()];
-        int i=0;
-        for(int el:set){
-            arr[i++]=el;
+        int s =lt.size();
+        int[] arr= new int[s];
+        for(int i=0;i<s;i++){
+            arr[i] = lt.get(i);
         }
-
-        Arrays.sort(arr);
-
         return arr;
     }
 }
