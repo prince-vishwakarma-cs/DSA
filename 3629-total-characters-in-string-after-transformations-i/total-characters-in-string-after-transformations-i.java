bb4dd6441259854c62f194266b4bfa6cbb30e00e
Prince Vishwakarma <Pv913177@gmail.com>
2025-05-13 13:36:49 +0530

    Time: 249 ms (34.18%) | Memory: 45.3 MB (91.14%) - LeetSync

--- 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
+++ 3629-total-characters-in-string-after-transformations-i/total-characters-in-string-after-transformations-i.java
@@ -2,22 +2,26 @@ class Solution {
     int modulo = 1000000007;
     public int lengthAfterTransformations(String s, int t) {
         int n =s.length();
-        long[][] dp = new long[26][t+1];
+        long[] dp = new long[26];
+        long[] temp=new long[26];
         long ans =0L;
         for(int i=0;i<26;i++){
-            dp[i][0]=1;
-        } 
+            dp[i]=1;
+        }
         for(int i=1;i<=t;i++){
             for(int j=0;j<26;j++){
                 if(j==25){
-            dp[j][i] = (dp[0][i-1]%modulo+dp[1][i-1]%modulo)%modulo;
+            temp[j] = (dp[0]%modulo+dp[1]%modulo)%modulo;
             continue;
         }
-        dp[j][i]= dp[j+1][i-1]%modulo;
+        temp[j]= dp[j+1]%modulo;
+            }
+            for(int j=0;j<26;j++){
+                dp[j]=temp[j];
             }
         }
         for(int i=0;i<n;i++){
-            ans=(ans+dp[s.charAt(i)-'a'][t])%modulo;
+            ans=(ans+dp[s.charAt(i)-'a'])%modulo;
         }
         return (int)ans;
     }
