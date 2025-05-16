11b721f79efe9afca35c9f6b408583d19f0a7e99
Prince Vishwakarma <Pv913177@gmail.com>
2025-05-16 21:58:05 +0530

    Time: 149 ms (16.95%) | Memory: 45.4 MB (82.37%) - LeetSync

--- 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
+++ 2317-count-collisions-on-a-road/count-collisions-on-a-road.java
@@ -1,25 +1,28 @@
 class Solution {
     public int countCollisions(String directions) {
-
+        int n =directions.length();
         int collision = 0;
 
         Stack<Character> st = new Stack<>();
 
-        for(int i=0; i<directions.length(); i++){
+        for(int i=0; i<n; i++){
 
             char ch = directions.charAt(i);
 
             if(st.isEmpty()){
                 st.push(ch);
-            } else if(st.peek()=='R' && ch=='L'){ // R-L collision
+                continue;
+            } 
+            char top =st.peek();
+            if(top=='R' && ch=='L'){ // R-L collision
                 st.pop();
                 st.push('S');
                 collision+=2;
-            } else if(st.peek()=='R' && ch=='S'){ // R-S collision
+            } else if(top=='R' && ch=='S'){ // R-S collision
                 st.pop();
                 st.push(ch);
                 collision+=1;
-            } else if(st.peek()=='S' && ch=='L') { // S-L collision
+            } else if(top=='S' && ch=='L') { // S-L collision
                 collision+=1;
             } else {
                 // no collison
@@ -27,8 +30,9 @@ class Solution {
             }
         }
 
-        while(st.size()>1){
+        while(!st.isEmpty()){
             char top = st.pop();
+            if(st.isEmpty()) break;
             if(st.peek()=='R' && top =='S'){
                 collision+=1;
                 st.pop();
