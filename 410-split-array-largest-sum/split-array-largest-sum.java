3c586e14ed2012881ab2d901fcdfe78343703fd8
Prince Vishwakarma <Pv913177@gmail.com>
2025-05-17 13:21:33 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (54.79%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,19 +1,34 @@
 class Solution {
-    public int solve(int[] nums,int k,int start,int end,Integer[][] dp){
-        if(k==1) return nums[end]-(start>0 ? nums[start-1]: 0);
-        int min=Integer.MAX_VALUE;
-        if(dp[start][k]!=null) return dp[start][k];
-         for(int i=start;i<end;i++){
-            int sum=nums[i]-(start>0 ? nums[start-1]: 0);
-            min=Math.min(min,Math.max(solve(nums,k-1,i+1,end,dp),sum));
-        }
-        return dp[start][k]=min;
-    }
     public int splitArray(int[] nums, int k) {
-        for(int i=1;i<nums.length;i++){
-            nums[i]+=nums[i-1];
+        int i=0;
+        int j=0;
+        for(int s=0;s<nums.length;s++){
+            i=Math.max(i,nums[s]);
+            j+=nums[s];
         }
-        Integer[][] dp=new Integer[nums.length][k+1];
-        return solve(nums,k,0,nums.length-1,dp);
+        int ans =0;
+        while(i<=j){
+            int mid =(i+j)>>1;
+            if(splits(nums,mid)<=k){
+                ans = mid;
+                j=mid-1;
+            }else{
+                i=mid+1;
+            }
+        }   
+        return ans;
+    }
+    public int splits(int[] nums,int s){
+        int n =nums.length;
+        int split =0;
+        int curr=0;
+        for(int i=0;i<n;i++){
+            curr+=nums[i];
+            if(curr>s){
+                split++;
+                curr=nums[i];
+            }
+        }
+        return split+1;
     }
 }
