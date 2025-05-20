9b8933dccfe5654a4494003a0c7d0c5eafb6cc62
Prince Vishwakarma <Pv913177@gmail.com>
2025-05-20 17:04:07 +0530

    Time: 3 ms (79.52%) | Memory: 96.2 MB (17.22%) - LeetSync

--- 3639-zero-array-transformation-i/zero-array-transformation-i.java
+++ 3639-zero-array-transformation-i/zero-array-transformation-i.java
@@ -1,22 +1,19 @@
 class Solution {
     public boolean isZeroArray(int[] nums, int[][] queries) {
         int n = nums.length;
-        int[] rangeCount = new int[n + 1]; // Extra space for prefix calculation
-
-        // Mark the start and end of each query range
+        int[] rangeCount = new int[n + 1];
         for (int[] q : queries) {
-            rangeCount[q[0]]++;           // Increment at the start of the range
-            if (q[1] + 1 < n) rangeCount[q[1] + 1]--; // Decrement after the range
+            rangeCount[q[0]]++;
+            if (q[1] + 1 < n) rangeCount[q[1] + 1]--;
         }
 
-        // Compute the prefix sum to get the count of queries affecting each index
         int activeQueries = 0;
         for (int i = 0; i < n; i++) {
             activeQueries += rangeCount[i];
             if (activeQueries >= nums[i]) {
-                nums[i] = 0; // Set to zero if condition is met
+                nums[i] = 0;
             }
-            if (nums[i] != 0) return false; // Early return if condition fails
+            if (nums[i] != 0) return false;
         }
         return true;
     }
