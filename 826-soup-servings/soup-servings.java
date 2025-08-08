31351a61f7e523c81bcb92e5442168e8e41d286a
Prince Vishwakarma <Pv913177@gmail.com>
2025-08-08 22:25:33 +0530

    Time: 2 ms (77.25%) | Memory: 41.2 MB (95.21%) - LeetSync

---
+++ 826-soup-servings/soup-servings.java
@@ -0,0 +1,43 @@
+class Solution {
+    private double[][] memo;
+
+    private double func(int a, int b) {
+
+        if (a <= 0 && b > 0) {
+            return 1;
+        }
+
+        if (a <= 0 && b <= 0) {
+            return 0.5;
+        }
+
+        if (b <= 0 && a >= 0) {
+            return 0;
+        }
+
+        if (memo[a][b] != -1) return memo[a][b];
+
+        double ch1 = func(a - 4, b - 0);
+        double ch2 = func(a - 3, b - 1);
+        double ch3 = func(a - 2, b - 2);
+        double ch4 = func(a - 1, b - 3);
+
+        return memo[a][b] = 0.25 * (ch1 + ch2 + ch3 + ch4);
+    }
+
+    public double soupServings(int n) {
+
+        if (n > 4800) return 1;
+
+        int y = (int) Math.ceil((double) n / 25.0);
+
+        memo = new double[y + 1][y + 1];
+        for (int i = 0; i <= y; i++) {
+            for (int j = 0; j <= y; j++) {
+                memo[i][j] = -1;
+            }
+        }
+
+        return func(y, y);
+    }
+}
