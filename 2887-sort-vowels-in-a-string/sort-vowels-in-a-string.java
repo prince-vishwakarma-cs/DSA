11c942e952076b5b8a8939f9111a435661965d50
Prince Vishwakarma <Pv913177@gmail.com>
2025-09-11 09:23:45 +0530

    Time: 56 ms (43.09%) | Memory: 48.2 MB (33.44%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String sortVowels(String s) {
+        List<Character> vowels = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                vowels.add(c);
+            }
+        }
+        Collections.sort(vowels);
+        StringBuilder result = new StringBuilder();
+        int vowelIndex = 0;
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                result.append(vowels.get(vowelIndex));
+                vowelIndex++;
+            } else {
+                result.append(c);
+            }
+        }
+
+        return result.toString();
+    }
+     
+    private boolean isVowel(char c) {
+        return "AEIOUaeiou".indexOf(c) != -1;
+    }
+}
