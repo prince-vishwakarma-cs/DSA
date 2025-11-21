ea5aa1933ac0df45e615cdd6ef2fe8be14a0ed5d
Prince Vishwakarma <Pv913177@gmail.com>
2025-11-21 09:13:07 +0530

    Time: 235 ms (18.54%) | Memory: 47 MB (23.40%) - LeetSync

--- 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -5,17 +5,17 @@ class Solution {
             R[u - 'a']++;
         }
         int[] L = new int[26];
-        HashSet<Integer> S = new HashSet<>();
+        HashSet<Integer> T = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             int t = s.charAt(i) - 'a';
             R[t]--;
             for (int j = 0; j < 26; j++) {
                 if (L[j] > 0 && R[j] > 0) {
-                    S.add(26 * t + j);
+                    T.add(26 * t + j);
                 }
             }
             L[t]++;
         }
-        return S.size();
+        return T.size();
     }
 }
