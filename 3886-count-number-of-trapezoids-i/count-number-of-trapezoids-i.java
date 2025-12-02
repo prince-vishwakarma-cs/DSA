78b065632e824f6a6f66696a8db69a523d8ecfc9
Prince Vishwakarma <Pv913177@gmail.com>
2025-12-02 09:19:00 +0530

    Time: 38 ms (73.47%) | Memory: 206.9 MB (18.37%) - LeetSync

---
+++ 3886-count-number-of-trapezoids-i/count-number-of-trapezoids-i.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int countTrapezoids(int[][] points) {
+        long MOD = 1_000_000_007L;
+        Map<Integer, Long> map = new HashMap<>();
+        for (int[] p : points) {
+            map.put(p[1], map.getOrDefault(p[1], 0L) + 1);
+        }
+        List<Long> seg = new ArrayList<>();
+        for (long k : map.values()) {
+            if (k >= 2) seg.add((k * (k - 1) / 2) % MOD);
+        }
+        long sum = 0, ans = 0;
+        for (long v : seg) {
+            ans = (ans + v * sum) % MOD;
+            sum = (sum + v) % MOD;
+        }
+        return (int) ans;
+    }
+}
